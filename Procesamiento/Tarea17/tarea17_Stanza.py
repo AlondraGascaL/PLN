73e8205114b7bb84cf2e7906b2544590aff80073
AlondraGascaL <aloogascaleon42@gmail.com>
2022-05-07 23:43:53 -0500

    17

---
+++ Procesamiento/Tarea17/tarea17_Stanza.py
@@ -0,0 +1,33 @@
+import stanza
+#Nos ayuda para expresiones regualres 
+import re
+
+stanza.download('es')
+nlp = stanza.Pipeline(lang='es', processors='tokenize,mwt,pos,lemma,depparse')
+
+
+##*****************Leer Archivo de texto **************************
+f = open (r'texto3.txt',encoding="utf8")
+texto1 = f.read()
+##print(textoLower)
+f.close()
+
+
+
+
+doc = nlp('Juan lee libro')
+listaAux = {}
+
+
+for sent in doc.sentences: 
+    for word in sent.words:
+        if word.head > 0:
+            aux = word.deprel+'('+sent.words[word.head-1].text+'-'+str(word.head)+','+word.text+'-'+str(word.id)+')'
+            listaAux[word.id-1] = aux
+        else:
+            aux = word.deprel+'('+"root"+'-'+str(word.head)+','+word.text+'-'+str(word.id)+')'
+            listaAux[word.id-1] = aux
+        
+with open("prueba.txt", 'w') as f: 
+    for value in listaAux.values(): 
+        f.write('%s\n' % value)
